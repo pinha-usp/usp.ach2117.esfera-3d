d7f2a3dcf4725111ade4726a5abd827c336fe6ee
Willian Pinheiro <willpinhaaa@gmail.com>
2023-05-11 20:22:23 -0300

    Implementa esfera

--- resources/shaders/default.vert
+++ resources/shaders/default.vert
@@ -1,4 +1,11 @@
 #version 330
 
+in vec3 in_vert;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 perspective;
+
 void main() {
+    gl_Position = perspective * view * model * vec4(in_vert, 1.0);
 }
--- window.py
+++ window.py
@@ -1,15 +1,56 @@
 from pathlib import Path
+import glm
 import moderngl_window as mglw
+from moderngl_window.opengl.vao import VAO
+from geometry import Sphere
 
 class Window(mglw.WindowConfig):
 
     gl_version = (3, 3)
     title = "Sphere"
     fullscreen = True
-    resource_dir = Path("resources") 
+    resource_dir = Path("resources")
+    aspect_ratio = 1.0
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+        self.program = self.load_program(
+            vertex_shader="shaders/default.vert",
+            fragment_shader="shaders/default.frag",
+        )
+
+        self.sphere = Sphere(depth = 5)
+
+        self.vao = VAO("sphere", mode = self.ctx.POINTS)
+        self.vao.buffer(self.sphere.vertices, "3f", "in_vert")
+
+        self.program["perspective"].write(
+            glm.perspective(
+                glm.radians(45),
+                1.0,
+                0.1,
+                100.0
+            )
+        )
+
     def render(self, time, frametime):
         self.ctx.clear()
+
+        self.program["model"].write(
+            glm.rotate(
+                glm.mat4(1.0),
+                glm.radians(time * 45),
+                glm.vec3(0, 1, 0)
+            )
+        )
+
+        self.program["view"].write(
+            glm.lookAt(
+                glm.vec3(2, 2, 2),
+                glm.vec3(0, 0, 0),
+                glm.vec3(0, 1, 0)
+            )
+        )
+
+        self.vao.render(self.program)
